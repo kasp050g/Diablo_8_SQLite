d660583a8c6cf235a02c1441ed3fdd1e855b6ae7
AsmundJuel <asmu0499@student.eadania.dk>
2020-04-27 19:31:12 +0200

    Update Asmund_Test_Scene.cs

--- Diablo_8_SQLite/Diablo_8_SQLite/Script/Diablo_8_TheGame/Scene/Asmund_Test_Scene.cs
+++ Diablo_8_SQLite/Diablo_8_SQLite/Script/Diablo_8_TheGame/Scene/Asmund_Test_Scene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonogameFramework;
 
@@ -19,6 +20,7 @@ namespace Diablo_8_SQLite
         public override void Initialize()
         {
             base.Initialize();
+            AsmundTest();
         }
 
         public override void OnSwitchAwayFromThisScene()
@@ -34,6 +36,36 @@ namespace Diablo_8_SQLite
         public override void Update()
         {
             base.Update();
+        }
+        private void AsmundTest()
+        {
+            GameObject go = new GameObject();
+            //go.AddComponent<SpriteRenderer>();
+
+            SpriteRenderer sp = new SpriteRenderer("Pixel", OriginPositionEnum.TopMid, 1, Color.White);
+            go.AddComponent<SpriteRenderer>(sp);
+
+            go.Transform.Scale = new Vector2(100, GraphicsSetting.Instance.ScreenSize.Y);
+            go.Transform.Position = new Vector2(GraphicsSetting.Instance.ScreenSize.X/2, 0);
+
+            ImageGUI bob = go.AddComponent<ImageGUI>(new ImageGUI(sp, false, false));
+            
+            Instantiate(go);
+
+            GameObject button = new GameObject();
+            button.AddComponent<SpriteRenderer>();
+            ButtonGUI btn1 = button.AddComponent<ButtonGUI>(new ButtonGUI(SpriteContainer.Instance.sprite["Pixel"], SpriteContainer.Instance.sprite["Pixel"], Color.White, Color.Green));
+            btn1.OnClick = () => { go.IsActive = !go.IsActive; };
+            button.Transform.Scale = new Vector2(300, 300);
+
+            Instantiate(button);
+
+            MouseSettings.Instance.IsMouseVisible(true);
+            //go.AddComponent<SpriteRenderer>(new SpriteRenderer("Pixel", OriginPositionEnum.Mid, 1, Color.White));
+
+
+            //SpriteRenderer lol = go.GetComponent<SpriteRenderer>();
+
         }
     }
 }

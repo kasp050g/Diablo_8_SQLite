850d77b5b5de1eab682bf27dbe4ace6a003a1ba9
Kasper Home <Kasper_fs@hotmail.com>
2020-04-29 11:56:57 +0200

    Jamen

--- Diablo_8_SQLite/Diablo_8_SQLite/Script/Diablo_8_TheGame/Scene/Befor_Game_Start/MakeUserGameObject.cs
+++ Diablo_8_SQLite/Diablo_8_SQLite/Script/Diablo_8_TheGame/Scene/Befor_Game_Start/MakeUserGameObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonogameFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace Diablo_8_SQLite
 {
     public class MakeUserGameObject
     {
+        GameObject userInput = new GameObject();
+        SpriteRenderer sr01;
+        InputFieldGUI if01;
 
+        GameObject emailInput = new GameObject();
+        SpriteRenderer sr02;
+        InputFieldGUI if02;
+
+        GameObject passwordInput01 = new GameObject();
+        SpriteRenderer sr03;
+        InputFieldGUI if03;
+
+        GameObject passwordInput02 = new GameObject();
+        SpriteRenderer sr04;
+        InputFieldGUI if04;
     }
 }
